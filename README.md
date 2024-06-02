757acfa508831f11629d7c32d7e71416f908673f
dinujpranesh <167699866+dinujpranesh@users.noreply.github.com>
2024-06-02 08:11:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AIEnchancedPHPDeveloper-G1AIEnchancedPHPDeveloper-G1
+AIEnchancedPHPDeveloper-G1AIEnchancedPHPDeveloper-G1
+
+This is the main code repository for our project
+ 
---
+++ dbregister.php
@@ -0,0 +1,32 @@
+<?php
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aiphp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the submitted data
+$email = $_POST['Email'];
+$firstName = $_POST['FirstName'];
+$lastName = $_POST['LastName'];
+$phone = $_POST['Phone'];
+$salary = $_POST['Salary'];
+$dateOfBirth = $_POST['DateOfBirth'];
+
+// Insert the data into the employee table
+$sql = "INSERT INTO employee (email, first_name, last_name, phone, salary, date_of_birth)
+VALUES ('$email', '$firstName', '$lastName', '$phone', '$salary', '$dateOfBirth')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error inserting data: " . $conn->error;
+}
+
+$conn->close();
+?>
